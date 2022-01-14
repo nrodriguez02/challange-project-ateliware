b78b6f19fd4283c92fd4746b5a52d6ef7e703d9b
Nestor Rodriguez <nrodriguez02@gmail.com>
2022-01-14 20:29:42 -0300

    fixing issues uwsgi.ini

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from sqlalchemy_utils import database_exists
 from config import create_app, setup_database
 from flask_bootstrap import Bootstrap5
 
+app = create_app()
+app.app_context().push()
+bootstrap = Bootstrap5(app)
+
 if __name__ == '__main__':
-    app = create_app()
-    app.app_context().push()
-    bootstrap = Bootstrap5(app)
     if database_exists(app.config["SQLALCHEMY_DATABASE_URI"]):
         setup_database(app)
     app.run(port=8001, debug=True)
